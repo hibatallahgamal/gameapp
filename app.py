bd4a8d934996dbef3bd9966850a8e32b5569c7b8
GAMAL Hibatallah <gmlhibatallah@gmail.com>
2021-05-22 01:04:28 +0200

    init file

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+        if request.method == "POST":
+                return redirect(url_for('game'))
         return render_template("index.html")
 
-@app.route("/game", methods=["POST","GET"])
-def game():
-        return render_template("game.html")
 
-if __name__=="__main__":
-    app.run (debug=True,port=5008)
+
+@app.route('/game', methods=['POST', 'GET'])
+def game():
+        return render_template("gaaaame.html")
+
+if __name__ == "__main__":
+    app.run (debug=True, port=5008)
